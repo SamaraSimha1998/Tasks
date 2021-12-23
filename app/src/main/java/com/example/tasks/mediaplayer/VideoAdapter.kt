bc6f9b2525e99e3220dd1c9510369905573c0d8e
SamaraSimha1998 <samaras@aapoon.net>
2021-12-23 16:38:28 +0530

    Initial commit

--- app/src/main/java/com/example/tasks/mediaplayer/VideoAdapter.kt
+++ app/src/main/java/com/example/tasks/mediaplayer/VideoAdapter.kt
@@ -6,15 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.tasks.R
 import java.util.ArrayList
+import android.net.Uri
+import android.widget.MediaController
+
+import android.widget.VideoView
+
+
+
 
 
 class VideoAdapter(var context: Context, var videoArrayList: ArrayList<VideoModel>) :
     RecyclerView.Adapter<VideoAdapter.viewHolder>() {
     var onItemClickListener: Any? = null
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): viewHolder {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.activity_video_list, viewGroup, false)
+        val view: View = LayoutInflater.from(context).inflate(com.example.tasks.R.layout.activity_video_list, viewGroup, false)
         return viewHolder(view)
     }
 
@@ -29,8 +35,8 @@ class VideoAdapter(var context: Context, var videoArrayList: ArrayList<VideoMode
     }
 
     inner class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var title: TextView = itemView.findViewById<View>(R.id.title) as TextView
-        var duration: TextView = itemView.findViewById<View>(R.id.duration) as TextView
+        var title: TextView = itemView.findViewById<View>(com.example.tasks.R.id.title) as TextView
+        var duration: TextView = itemView.findViewById<View>(com.example.tasks.R.id.duration) as TextView
 
         init {
             itemView.setOnClickListener { v ->
@@ -49,5 +55,17 @@ class VideoAdapter(var context: Context, var videoArrayList: ArrayList<VideoMode
 }
 
 private fun Any?.onItemClick(pos: Int, v: View?) {
-    TODO("Not yet implemented")
+
+    // Here we can implement code to play the video
+//    TODO("Not yet implemented")
+    val videoView = (com.example.tasks.R.id.videoView) as VideoView
+//Use a media controller so that you can scroll the video contents
+//and also to pause, start the video.
+//Use a media controller so that you can scroll the video contents
+//and also to pause, start the video.
+    val mediaController = MediaController(this as Context?)
+    mediaController.setAnchorView(videoView)
+    videoView.setMediaController(mediaController)
+    videoView.setVideoURI(Uri.parse(this as String?))
+    videoView.start()
 }
