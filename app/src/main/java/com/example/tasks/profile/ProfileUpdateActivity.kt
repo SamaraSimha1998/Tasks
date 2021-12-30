87c864e83dc7a2794d7f8c3fa8a88ec9595cb6f6
SamaraSimha1998 <samaras@aapoon.net>
2021-12-30 15:50:26 +0530

    Initial commit

--- app/src/main/java/com/example/tasks/profile/ProfileUpdateActivity.kt
+++ app/src/main/java/com/example/tasks/profile/ProfileUpdateActivity.kt
@@ -1,10 +1,10 @@
 package com.example.tasks.profile
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.example.tasks.R
 
-class ProfileUpdate : AppCompatActivity() {
+class ProfileUpdateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_save)
