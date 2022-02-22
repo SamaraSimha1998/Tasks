477defd3b3c9255349e6f9e3841ca901eb44a7d8
SamaraSimha1998 <samaras@aapoon.net>
2022-02-22 10:55:32 +0530

    Initial commit

--- app/src/main/java/com/example/tasks/contacts/ContactsActivity.kt
+++ app/src/main/java/com/example/tasks/contacts/ContactsActivity.kt
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tasks.R
