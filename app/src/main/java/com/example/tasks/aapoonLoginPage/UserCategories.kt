fa231eb1e2e211a452caa6b465d1d9f1696932cc
SamaraSimha1998 <samaras@aapoon.net>
2022-03-05 17:23:53 +0530

    Initial commit

--- app/src/main/java/com/example/tasks/aapoonLoginPage/UserCategories.kt
+++ app/src/main/java/com/example/tasks/aapoonLoginPage/UserCategories.kt
@@ -1,12 +1,57 @@
 package com.example.tasks.aapoonLoginPage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tasks.R
+import com.example.tasks.profile.ProfileLogActivity
+import com.example.tasks.tabProfile.TabProfileActivity
+import kotlinx.android.synthetic.main.activity_user_categories.*
 
 class UserCategories : AppCompatActivity() {
+
+    private lateinit var radioButton: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_categories)
+
+        btn_continue.setOnClickListener {
+            when (radioButton) {
+                "btn_business_user_radio" -> {
+                    val intent = Intent(this,ProfileLogActivity::class.java)
+                    startActivity(intent)
+                }
+                "btn_individual_user_radio" -> {
+                    val intent = Intent(this,TabProfileActivity::class.java)
+                    startActivity(intent)
+                }
+                else -> {
+                    Toast.makeText(this,"Please select any option",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.btn_business_user_radio ->
+                    if (checked) {
+                        radioButton = "btn_business_user_radio"
+                    }
+                R.id.btn_individual_user_radio ->
+                    if (checked) {
+                        radioButton = "btn_individual_user_radio"
+                    }
+            }
+        }
     }
 }
