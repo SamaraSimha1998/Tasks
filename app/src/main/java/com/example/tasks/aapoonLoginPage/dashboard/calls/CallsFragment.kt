255eb98c5f3b27e8bfa2a48852c875b804daf228
SamaraSimha1998 <samaras@aapoon.net>
2022-03-22 10:24:51 +0530

    Initial commit

--- app/src/main/java/com/example/tasks/aapoonLoginPage/dashboard/calls/CallsFragment.kt
+++ app/src/main/java/com/example/tasks/aapoonLoginPage/dashboard/calls/CallsFragment.kt
@@ -1,7 +1,20 @@
 package com.example.tasks.aapoonLoginPage.dashboard.calls
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.tasks.R
 
 class CallsFragment : Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view: View = inflater.inflate(R.layout.fragment_calls, container, false)
+        // calls are shown here
+        return view
+    }
 }
